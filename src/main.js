9c44d53c0014fa5e2b319552639a01db3366e45f
voidstudios07-netizen <voidstudios07@gmail.com>
2026-07-10 13:27:03 +0530

    Delete src/main.js

--- src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-var client_1 = require("react-dom/client");
-require("./index.css");
-var App_tsx_1 = require("./App.tsx");
-(0, client_1.createRoot)(document.getElementById('root')).render(<react_1.StrictMode>
-    <App_tsx_1.default />
-  </react_1.StrictMode>);
